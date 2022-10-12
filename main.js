be92e7c83a736b10e8ad7a7595109ba9360e8e8a
lauraonetwo <yiwang03@ucm.es>
2022-10-12 19:31:03 +0200

    añadido comentarios en main.js

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
+// Uso de la libreria Phaser 5
+
+// Importar al player, enemigos, etc.
+// import ...
+
+// Cuando la ventana cargue...
+// Realizar la recogida de datos, inicializacion, bucle de juego, etc.
 window.onload = function()
 {
-	var width = window.screen.width * window.devicePixelRatio - 100;
+	/*var width = window.screen.width * window.devicePixelRatio - 100;
 	var height = window.screen.height * window.devicePixelRatio - 200;
 	console.log(width,height);
 	document.getElementById("pagina").style.width = width + "px";
-	document.getElementById("pagina").style.height = height + "px";
+	document.getElementById("pagina").style.height = height + "px";*/
 };
+
