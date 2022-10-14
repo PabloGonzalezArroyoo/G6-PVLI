bfd722664901fd3a8c5d5f977689806fec656636
PedroSanchezPresidente <pedro.sanchez.vela72@gmail.com>
2022-10-14 18:56:05 +0200

    Plantilla del html

--- main.js
+++ main.js
@@ -7,10 +7,12 @@
 // Realizar la recogida de datos, inicializacion, bucle de juego, etc.
 window.onload = function()
 {
-	/*var width = window.screen.width * window.devicePixelRatio - 100;
-	var height = window.screen.height * window.devicePixelRatio - 200;
+	//Coge la resolucion de la pantalla del ordenador
+	var width = window.screen.width * window.devicePixelRatio;
+	var height = window.screen.height * window.devicePixelRatio;
 	console.log(width,height);
+	//Ajusta el body a la resolucion de la pantalla
 	document.getElementById("pagina").style.width = width + "px";
-	document.getElementById("pagina").style.height = height + "px";*/
+	document.getElementById("pagina").style.height = height + "px";
 };
 
