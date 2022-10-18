54b520f0b566117528a7b173957203c76c463550
Zorromatamoscas <99506562+Zorromatamoscas@users.noreply.github.com>
2022-10-18 12:13:57 +0200

    Primera implementación de boton (StarScene)

--- src/scenes/titleScene.js
+++ src/scenes/titleScene.js
@@ -2,6 +2,7 @@
  * Escena de Pantalla de Título.
  * @extends Phaser.Scene
  */
+import { Button } from "../button";
 export default class TitleScene extends Phaser.Scene {
 	/**
 	 * Escena principal.
@@ -15,6 +16,7 @@ export default class TitleScene extends Phaser.Scene {
 	 * Cargamos todos los assets que vamos a necesitar
 	 */
 	preload(){
+		//this.load.spritesheet("nombre_spritesheet", "direccion_spritesheet",{ frameWidth: x, frameHeight: y }); Carga la SpriteSheet
 		this.load.image('start', 'assets/GUI/start.png');
 		//this.load.image('castle', 'assets/castle.gif');
 	}
@@ -27,26 +29,9 @@ export default class TitleScene extends Phaser.Scene {
 		//var back = this.add.image(0, 0, 'castle').setOrigin(0, 0);
 
 		//Pintamos un botón de Empezar
-		var sprite = this.add.image(this.sys.game.canvas.width/2, this.sys.game.canvas.height/2, 'start')
-		sprite.setInteractive(); // Hacemos el sprite interactivo para que lance eventos
-
-		// Escuchamos los eventos del ratón cuando interactual con nuestro sprite de "Start"
-	    sprite.on('pointerdown', pointer => {
-	    	// Marcar botón
-	    });
-
-	    sprite.on('pointerup', pointer => {
-			this.scene.start('cinematicScene'); //Cambiamos a la escena de la cinemática
-	    });
-
-		sprite.on('pointerover', () => {
-			// Destacar botón
-	    });
-
-	    sprite.on('pointerout', () => {
-			// Desdestacar botón
-		});
+		var button=new Button(this,'start',0,0,0,200,300,this.scene.start('cinematicScene'))
 		//Para seleccionar botones con teclas, creamos el objeto tecla
+		
 		var keys = this.scene.input.keyboard.addKeys('LEFT, UP, RIGHT,DOWN,W,A,S,D');
 		var Esc = this.scene.input.keyboard.addKeys('ESC,X');
 		var Enter = this.scene.input.keyboard.addKeys('ENTER,Z')
