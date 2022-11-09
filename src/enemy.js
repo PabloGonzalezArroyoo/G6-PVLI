e2cc1ebacdef545f30375362ed457b47daead369
MiguelCGa <micurros@ucm.es>
2022-11-09 13:09:28 +0100

    Habilidad Drake + optimización

--- src/enemy.js
+++ src/enemy.js
@@ -6,30 +6,40 @@ import HealthController from './healthController.js';
 export class Enemy extends Character {
     constructor(scene, x, y, spritesheet, maxHealth, damage) {
         if (scene !== null) super(x, y, new EnemyAnimator(scene, x, y, spritesheet), new HealthController(scene, x, y - 150, maxHealth), damage);
-        else super(x, y, null, null, damage)
+        else {
+            super(x, y, null, null, damage);
+            this.spritesheet = spritesheet;
+            this.maxHealth = maxHealth;
+        }
     }
     // método para añadir una escena si no tenía
-    setScene(scene, maxHealth, spritesheet){
-        this.healthController = new HealthController(scene, this.x, this.y - 150, maxHealth);
-        this.animator = new EnemyAnimator(scene, this.x, this.y, spritesheet);
+    setScene(scene) {
+        this.healthController = new HealthController(scene, this.x, this.y - 150, this.maxHealth);
+        this.animator = new EnemyAnimator(scene, this.x, this.y, this.spritesheet);
     }
-    attack(player){    
+    attack(player, abilityPercentage) {    
         // Animacion de ataque
         // this.animator.playAttack();
 
-        // Le baja vida al jugador y devolvuelve el daño hecho
+        // Hace la habilidad y devuelve el daño hecho
+        if (abilityPercentage)
+            if (Math.floor(Math.random() * 100) < abilityPercentage) 
+                return this.ability(player);
+
+        // Golpea y devuelve el daño hecho
         return player.receiveAttack(this.damage);
     }
-    getAnimator(){return this.animator;}
+
+    // Habilidad por defecto de lo enemigos, simplemente ataca sin probabilidad de llamar a la habilidad
+    ability(player) { return Enemy.prototype.attack.call(this, player); }
+
+    getAnimator() { return this.animator; }
 }
 
 // Enemigo 1
 export class DrunkRuffian extends Enemy {
     constructor(scene, x, y) {
         super(scene, x, y, 'drunkRuffian', 100, 15);
-    }
-    setScene(scene){
-        super.setScene(scene, 100, 'drunkRuffian');
     }
 }
 
@@ -38,9 +48,6 @@ export class DrunkRuffian extends Enemy {
     constructor(scene, x, y) {
         super(scene, x, y, 'stinkyPirate', 150, 25);
     }
-    setScene(scene){
-        super.setScene(scene, 150, 'stinkyPirate');
-    }
 }
 
 // Enemigo 3
@@ -48,15 +55,15 @@ export class ScurviedSailor extends Enemy {
     constructor(scene, x, y) {
         super(scene, x, y, 'scurviedSailor', 200, 35);
     }
-    setScene(scene){
-        super.setScene(scene, 200, 'scurviedSailor');
-    }
     
-    attack(player){
-        super.attack(player);
-        // envenenar
-        let bleed = Math.floor(Math.random() * 100) < 15;
-        if (bleed) player.bleedController.activateBleed(3, 2);
+    // Atacar con un 15% de probabilidad de hacer su habilidad
+    attack(player) {
+        return super.attack(player, 15)
+    }
+
+    ability(player) {
+        player.turnEffectController.activateBleed(3, 2);
+        return super.attack(player);
     }
 }
 
@@ -65,21 +72,22 @@ export class ScurviedSailor extends Enemy {
     constructor(scene, x, y) {
         super(scene, x, y, 'experiencedBuccaneer', 250, 45);
     }
-    setScene(scene){
-        super.setScene(scene, 250, 'experiencedBuccaneer');
+
+    attack(player) {
+        return super.attack(player, 15);
     }
 
-    attack(player){
-        // Calcular probabilidad de critico
-        let crit = Math.floor(Math.random() * 100) < 15;
-        // Calcular aumento de daño de critico
+    // Atacar con un 15% de probabilidad de hacer su habilidad
+    ability(player) {
         let critDmg = (this.damage/100)*40;
         // Aumentar el daño
-        if (crit) this.damage += critDmg;
+        this.damage += critDmg;
         // Atacar
-        super.attack(player);
+        let dmg = super.attack(player);
         // Restaurar el daño
-        if (crit) this.damage -= critDmg;
+        this.damage -= critDmg;
+        
+        return dmg;
     }
 }
 
@@ -88,15 +96,16 @@ export class ScurviedSailor extends Enemy {
     constructor(scene, x, y) {
         super(scene, x, y, 'alienatedCosair', 300, 55);
     }
-    setScene(scene){
-        super.setScene(scene, 300, 'alienatedCosair');
-    }
     
-    attack(player){
-        // Calcular probabilidad de cura
-        let heal = Math.floor(Math.random() * 100) < 15;
+    // Atacar con un 15% de probabilidad de hacer su habilidad
+    attack(player) {
+        return super.attack(player, 15);
+    }
+
+    ability(player) {
         let dmg = super.attack(player);
-        if (heal) this.healthController.changeHealth((dmg/100)*50);
+        this.healthController.changeHealth((dmg/100)*50);
+        return dmg;
     }
 }
 
@@ -105,20 +114,21 @@ export class ScurviedSailor extends Enemy {
     constructor(scene, x, y) {
         super(scene, x, y, 'ensignDrake', 500, 50);
     }
-    setScene(scene){
-        super.setScene(scene, 500, 'ensignDrake');
-    }
     
-    attack(player){
-        // cualquiera de las otras habilidades
-        // Calcular probabilidad de critico
-        let ability = true/*Math.floor(Math.random() * 100) < 20*/;
-        if (ability){
-            let rnd = 0/*Math.floor(Math.random() * 3)*/;
-            if (rnd == 0) console.log(ScurviedSailor.attack)/*ScurviedSailor.attack.apply(this, [player])*/;
-            if (rnd == 1) ExperiencedBuccaneer.attack.apply(this, [player]);
-            if (rnd == 2) AlienatedCosair.attack.apply(this, [player]);
-        }
-        else super.attack(player);
+    attack(player) {
+        // Atacar con un 20% de probabilidad de hacer su habilidad
+        return super.attack(player, 20);
+    }
+
+    ability(player) {
+        // Escoger una de las 3 habilidades aleatoriamente
+        let rnd = Math.floor(Math.random() * 3);
+        // Asignar tipo de enemigo para la habilidad
+        let enemyType;
+        if (rnd == 0) enemyType = ScurviedSailor;
+        else if (rnd == 1) enemyType = ExperiencedBuccaneer;
+        else if (rnd == 2) enemyType = AlienatedCosair;
+        // Habilidad
+        return enemyType.prototype.ability.call(this, player);
     }
 }
