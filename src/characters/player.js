939fb2875adf1694ad76a113d1aa3ea3cbba2e9a
MiguelCGa <micurros@ucm.es>
2022-11-22 12:02:51 +0100

    Que locura

--- src/characters/player.js
+++ src/characters/player.js
@@ -50,7 +50,7 @@ export default class Player extends Character {
 
     quelocura(enemies, index){
         console.log("QUELOCURA");
-        this.inventory.equippedWeapon.queLocura(this, enemies, index);
+        this.inventory.equipedWeapon.queLocura(this, enemies, index);
         this.healthController.scene.time.delayedCall(1000, () => {this.healthController.emitter.emit("finishTurn")});
     }
 
