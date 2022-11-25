824062be4b4ac6a46597aa8da272839ec8652357
lauraonetwo <yiwang03@ucm.es>
2022-11-25 17:57:30 +0100

    Update healthController.js

--- src/characters/healthController.js
+++ src/characters/healthController.js
@@ -6,20 +6,13 @@ export default class HealthController extends Phaser.GameObjects.Sprite {
         // Sprite en la UI
         super(scene, x, y, 'lifeBar');
 
-        this.barScaleX = 4;
-        this.barScaleY = 4;
-        this.colorBarScaleX = this.barScaleX * 9;
-        this.colorBarScaleY = this.barScaleY;
-
-        // Divisiones de la barra de vida
-        this.healthDivider = this.colorBarScaleX / maxHP;
-        
-        // Offset para la barra de color
-        this.offsetX = 0;
+        this.barScaleX = this.barScaleY = 4; // Cambiar este valor para cambiar el tamaño de la barra de vida
+        this.colorBarScaleX = this.barScaleX * 36;
+        this.colorBarScaleY = this.barScaleX * 5;
 
         // Añade barra de color escalada
-        // TAM SPRITE * SCALE
-        this.color = this.scene.add.image(x, y, 'lifeBarColors', 3).setScale(this.colorBarScaleX,this.colorBarScaleY);
+        this.color = 0x00FF00;
+        this.colorBar = this.scene.add.rectangle(x - this.colorBarScaleX/2, y - this.colorBarScaleY/2 , this.colorBarScaleX, this.colorBarScaleY, this.color).setOrigin(0,0);
         // Añade cuadro de la barra
         this.scene.add.existing(this).setScale(this.barScaleX,this.barScaleY);
 
@@ -33,52 +26,35 @@ export default class HealthController extends Phaser.GameObjects.Sprite {
 
         if (value != 0) {
             /* Lógica */
-            // MAXIMO
-            if (this.currentHealth + value > this.maxHealth) value = this.maxHealth - this.currentHealth;
-            // MINIMO
-            else if (this.currentHealth + value < 0){
-                this.color.destroy();
-                value = -this.currentHealth;
-            }
+            if (this.currentHealth + value > this.maxHealth) value = this.maxHealth - this.currentHealth; // MAXIMO
+            else if (this.currentHealth + value < 0) { value = -this.currentHealth; } // MINIMO
+
             this.currentHealth += value;
 
             /* Animación */
-            var frameNumber = 3;
             switch (true) {
                 // Colores
-                case this.currentHealth <= 75 && this.currentHealth > 50: {
-                    frameNumber = 2; break;
+                case this.currentHealth <= 0.75 * this.maxHealth && this.currentHealth > 0.50 * this.maxHealth: {
+                    this.color = 0xff8000; break; // Naranja
                 } 
-                case this.currentHealth <= 50 && this.currentHealth > 25: {
-                    frameNumber = 1; break;
+                case this.currentHealth <= 0.50 * this.maxHealth && this.currentHealth > 0.25 * this.maxHealth: {
+                    this.color = 0xffff00; break; // Amarillo
                 }
-                case this.currentHealth <= 25 && this.currentHealth > 0: {
-                    frameNumber = 0; break;
+                case this.currentHealth <= 0.25 * this.maxHealth && this.currentHealth > 0: {
+                    this.color = 0xff0000; break; // Rojo
                 }
             }
-
-            /* CALCULOS */
-            // La posicion X para la barra de color debe ir acumulando esos pixeles/2 ya que se quita por ambos lados
-            // Calcular los pixeles a quitar
-            var pix =  this.colorBarScaleX * this.currentHealth/this.maxHealth;
-            this.offsetX -= Math.round(pix / 2);
-            //console.log(this.offsetX);
-            const self = this;
             
             if (this.currentHealth > 0) {
                 /* Animación de la barra de vida */
                 this.scene.tweens.add({
-                    targets: self.color,
-                    scaleX: self.colorBarScaleX * (self.currentHealth/self.maxHealth),
-                    x: self.x + self.offsetX,
+                    targets: this.colorBar,
+                    width: this.colorBarScaleX * (this.currentHealth/this.maxHealth),
+                    fillColor: this.color,
                     ease: 'Linear',
                     duration: 2000,
                 });
-                //console.log(self.color.x);
             }
-            
-            // Añade el cuadro de la barra
-            this.scene.add.image(this.x, this.y, 'lifeBar').setDisplaySize(this._barDisplaySizeX, this._barDisplaySizeY);
         }
         else {
             console.log("NO HA CAMBIADO LA VIDA")
