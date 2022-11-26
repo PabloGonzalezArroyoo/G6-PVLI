128ef0b607a13962949b8f7041f8d6de2b3791e1
Pavipollo17 <pagonz30@ucm.es>
2022-11-26 19:05:07 +0100

    Update listOfLevels.js

--- src/data/listOfLevels.js
+++ src/data/listOfLevels.js
@@ -1,105 +1,15 @@
 export var listOfLevels = [
-	//Level 1
+	// Level 1
 	{
-		x: 272,
-		y: 527.5,
-		enemies: [
-			{id: "drunkRuffian", x: 720, y: 200}
-			],
-		loot: []
-	},
-	//Level 2
-	{
-		x: 354,
-		y: 455.5,
+		x: 814,
+		y: 261,
 		enemies: [
 			{id: "drunkRuffian", x: 700, y: 200},
 			{id: "drunkRuffian", x: 750, y: 250}
 			],
 		loot: [],
 	},
-	//Level 3
-	{
-		x: 292,
-		y: 363.5,
-		enemies: [
-			{id: "drunkRuffian", x: 700, y: 200},
-			{id: "drunkRuffian", x: 750, y: 250}
-			],
-		loot: [],
-	},
-	//Level 4
-	{
-		x: 405,
-		y: 363.5,
-		enemies: [
-			{id: "drunkRuffian", x: 700, y: 200},
-			{id: "drunkRuffian", x: 750, y: 250}
-			],
-		loot: [],
-	},
-	//Level 5
-	{
-		x: 354,
-		y: 271.5,
-		enemies: [
-			{id: "drunkRuffian", x: 700, y: 200},
-			{id: "drunkRuffian", x: 750, y: 250}
-			],
-		loot: [],
-	},
-	//Level 6
-	{
-		x: 507,
-		y: 302,
-		enemies: [
-			{id: "drunkRuffian", x: 700, y: 200},
-			{id: "drunkRuffian", x: 750, y: 250}
-			],
-		loot: [],
-	},
-	//Level 7
-	{
-		x: 548,
-		y: 394.5,
-		enemies: [
-			{id: "drunkRuffian", x: 700, y: 200},
-			{id: "drunkRuffian", x: 750, y: 250}
-			],
-		loot: [],
-	},
-	//Level 8
-	{
-		x: 589,
-		y: 486.5,
-		enemies: [
-			{id: "drunkRuffian", x: 700, y: 200},
-			{id: "drunkRuffian", x: 750, y: 250}
-			],
-		loot: [],
-	},
-
-	//Level 9
-	{
-		x: 630,
-		y: 353.5,
-		enemies: [
-			{id: "drunkRuffian", x: 700, y: 200},
-			{id: "drunkRuffian", x: 750, y: 250}
-			],
-		loot: [],
-	},
-	//Level 10
-	{
-		x: 610,
-		y: 240.5,
-		enemies: [
-			{id: "drunkRuffian", x: 700, y: 200},
-			{id: "drunkRuffian", x: 750, y: 250}
-			],
-		loot: [],
-	},
-	//Level 11
+	// Level 2
 	{
 		x: 702,
 		y: 261,
@@ -109,14 +19,104 @@ export var listOfLevels = [
 			],
 		loot: [],
 	},
-	//Level 12
+	// Level 3
 	{
-		x: 814,
-		y: 261,
+		x: 630,
+		y: 353.5,
 		enemies: [
 			{id: "drunkRuffian", x: 700, y: 200},
 			{id: "drunkRuffian", x: 750, y: 250}
 			],
 		loot: [],
+	},
+	// Level 4
+	{
+		x: 610,
+		y: 240.5,
+		enemies: [
+			{id: "drunkRuffian", x: 700, y: 200},
+			{id: "drunkRuffian", x: 750, y: 250}
+			],
+		loot: [],
+	},
+	// Level 5
+	{
+		x: 548,
+		y: 394.5,
+		enemies: [
+			{id: "drunkRuffian", x: 700, y: 200},
+			{id: "drunkRuffian", x: 750, y: 250}
+			],
+		loot: [],
+	},
+	// Level 6
+	{
+		x: 589,
+		y: 486.5,
+		enemies: [
+			{id: "drunkRuffian", x: 700, y: 200},
+			{id: "drunkRuffian", x: 750, y: 250}
+			],
+		loot: [],
+	},
+	// Level 7
+	{
+		x: 507,
+		y: 302,
+		enemies: [
+			{id: "drunkRuffian", x: 700, y: 200},
+			{id: "drunkRuffian", x: 750, y: 250}
+			],
+		loot: [],
+	},
+	// Level 8
+	{
+		x: 405,
+		y: 363.5,
+		enemies: [
+			{id: "drunkRuffian", x: 700, y: 200},
+			{id: "drunkRuffian", x: 750, y: 250}
+			],
+		loot: [],
+	},
+
+	// Level 9
+	{
+		x: 354,
+		y: 271.5,
+		enemies: [
+			{id: "drunkRuffian", x: 700, y: 200},
+			{id: "drunkRuffian", x: 750, y: 250}
+			],
+		loot: [],
+	},
+	// Level 10
+	{
+		x: 354,
+		y: 455.5,
+		enemies: [
+			{id: "drunkRuffian", x: 700, y: 200},
+			{id: "drunkRuffian", x: 750, y: 250}
+			],
+		loot: [],
+	},
+	// Level 11
+	{
+		x: 292,
+		y: 363.5,
+		enemies: [
+			{id: "drunkRuffian", x: 700, y: 200},
+			{id: "drunkRuffian", x: 750, y: 250}
+			],
+		loot: [],
+	},
+	// Level 12
+	{
+		x: 272,
+		y: 527.5,
+		enemies: [
+			{id: "drunkRuffian", x: 720, y: 200}
+			],
+		loot: []
 	}
 ]
